dd8e6d6d6361e690c33a28cb0dfbf47032e51ee6
firsty-rahma <53887929+firsty-rahma@users.noreply.github.com>
2024-08-21 16:21:39 +0700

    Adding password strength logic

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -9,3 +9,21 @@ export function generatePassword(length) {
 
     return password
 }
+
+export function powerPassword(password) {
+    let power = document.getElementById('power-point');
+    let point = 0;
+    let widthPower = ["1%", "25%", "50%", "75%", "100%"];
+    let colorPower = ["#D73F40", "#DC6551", "#F2B84F", "#BDE952", "#3ba62f"];
+    let arrayTest = [/[0-9]/, /[a-z]/, /[A-Z]/, /W/];
+
+    arrayTest.forEach((item) => {
+        if (item.test(password)) {
+            point += 1;
+        }
+    });
+
+    power.style.width = widthPower[point];
+    power.style.backgroundColor = colorPower[point];
+    
+}
